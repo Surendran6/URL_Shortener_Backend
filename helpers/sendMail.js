a0a58a6a122993f59915ff6ac0ba054a3e84b533
surendran <surendransakthivel6@gmail.com>
2022-01-30 14:07:50 +0530

    update

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -3,35 +3,36 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = (options) => {
 
-    // const transporter = nodemailer.createTransport({
-    //     service : "Hotmail",
-    //     auth : {
-    //         user : "dran.customer96@hotmail.com",
-    //         pass : process.env.EMAIL_PASSWORD,
-    //     }
-    // })
-    // const mailDetails = {
-    //     from : "dran.customer96@hotmail.com",
-    //     to : options.to,
-    //     subject : options.subject,
-    //     html : options.text
-    // }
-    
-   
-    var transporter = nodemailer.createTransport({
-        host: "smtp-mail.outlook.com", // hostname
-        secureConnection: false, // TLS requires secureConnection to be false
-        port: 587, // port for secure SMTP
-        tls: {
-        ciphers:'SSLv3'
-        },
-        
-        requireTLS:true,
+    const transporter = nodemailer.createTransport({
+        service : "Hotmail",
         auth : {
             user : "dran.customer96@hotmail.com",
-            pass : process.env.EMAIL_PASSWORD 
+            pass : process.env.EMAIL_PASSWORD,
         }
     })
+    const mailDetails = {
+        from : "dran.customer96@hotmail.com",
+        to : options.to,
+        subject : options.subject,
+        html : options.text
+    }
+    
+   
+    // var transporter = nodemailer.createTransport({
+    //     host: "smtp-mail.outlook.com", // hostname
+    //     secureConnection: false, // TLS requires secureConnection to be false
+    //     port: 587, // port for secure SMTP
+    //     service : "Hotmail",
+    //     tls: {
+    //     ciphers:'SSLv3'
+    //     },
+        
+    //     requireTLS:true,
+    //     auth : {
+    //         user : "dran.customer96@hotmail.com",
+    //         pass : process.env.EMAIL_PASSWORD 
+    //     }
+    // })
 
 
     transporter.sendMail(mailDetails,function(err,data){
